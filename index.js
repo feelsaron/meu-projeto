f00b842be896c9e486abdb70b396591695689fff
feelsaron <pedrofreitas1128@gmail.com>
2022-09-26 22:14:25 -0300

    cria servidor localhost

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 console.log("hi!!")
 
 console.log("teste")
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, function (){
+    console.log("Servidor rodando no endereço http://localhost:3000");
+})
