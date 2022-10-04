3527748acc6759d91bae41135b25ff43591d48e0
feelsaron <pedrofreitas1128@gmail.com>
2022-10-03 21:56:49 -0300

    cria teste.html

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ console.log("hi!!")
 console.log("teste")
 
 const express = require('express');
+const path = require('path');
 const app = express();
+
+app.get('/', function (req, res){
+    res.send('Agora está funcionando a rota "/"');
+})
 
 app.listen(3000, function (){
     console.log("Servidor rodando no endereço http://localhost:3000");
-})
+});
+
+
+app.get('/index', function (req, res){
+    res.sendFile(path.join(__dirname, '/teste.html'));
+});
